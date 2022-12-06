21dca8b046c18c59d61a21935c177c8fd21e5df4
kyungbaa <cocoroto@naver.com>
2022-12-06 20:34:58 +0900

    STYLE: Navbar Header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,16 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <>
-      <header>
-        <Link to="/">
-          <h1>
-            <img src="./logo192.png" alt="shoppyHooHoo Logo" />
-            ShoppyHooHoo
-          </h1>
+      <header className="navbar navbar-expand-lg shadow-sm py-2 px-2 bg-white relative flex items-center w-full justify-between">
+        <Link
+          to="/"
+          className="flex items-center text-2xl font-bold text-brand"
+        >
+          <img src="./logo192.png" alt="HooHoo Logo" className="w-9" />
+          <h1>HooHoo</h1>
         </Link>
 
-        <ul>
+        <ul className="flex items-center gap-4 font-semibold">
           <Link to="/products">
             <li>products</li>
           </Link>
